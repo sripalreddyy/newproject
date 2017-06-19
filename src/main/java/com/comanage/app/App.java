2fe4fbe7f43a47d43b89b76fa86d2364976a29fc
Sripal Reddy <sripalreddies@gmail.com>
2017-06-19 11:45:17 +0530

    added new project

---
+++ src/main/java/com/comanage/app/App.java
@@ -0,0 +1,13 @@
+package com.comanage.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
